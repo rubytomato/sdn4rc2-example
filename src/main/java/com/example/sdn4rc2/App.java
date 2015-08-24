d5c35a1ce2c98da75268218801bbda48aa110e8d
rubytomato <vd4s-wtnb@asahi-net.or.jp>
2015-08-25 01:31:31 +0900

    first commit from eclipse

---
+++ src/main/java/com/example/sdn4rc2/App.java
@@ -0,0 +1,13 @@
+package com.example.sdn4rc2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
